a9e4aea5fef6904075148e4ec82ea9fcf4cfc0a1
doing-it-sideways <nohitruns73@gmail.com>
2025-03-25 04:53:29 -0400

    added bit header to core, constexpr values + function declaration for later usage in memory.hpp

--- include/Core.hpp
+++ include/Core.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <bit>
 #include <cstdint>
 
 namespace gb {
---
+++ include/Memory.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Core.hpp"
+
+namespace gb::mem {
+
+byte Read(u16 addr);
+
+} // namespace gb::mem

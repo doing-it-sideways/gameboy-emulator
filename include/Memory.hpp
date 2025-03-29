ad928f387a67c36435a3ccaac0634e51d58b0028
doing-it-sideways <nohitruns73@gmail.com>
2025-03-29 05:05:53 -0400

    extra info on memory header

--- include/Memory.hpp
+++ include/Memory.hpp
@@ -9,7 +9,9 @@ class Memory {
 public:
 	Memory(rom::RomData&& data);
 
-	// C++23 features that allow explicit object params to handle const/non-const in one func
+	// C++23 feature that allows explicit object params, which in this case,
+	// handles const/non-const in one function. Downside is because of auto,
+	// it has to be inlined, so implementation is included in .cxx
 	auto&& operator[](this auto&& self, u16 addr);
 
 private:

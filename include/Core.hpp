ab72eb9221bdb458d82c3824f10a13b84984f34b
doing-it-sideways <nohitruns73@gmail.com>
2025-03-29 04:40:35 -0400

    added core macros and u64 definition (used later in memory class)

--- include/Core.hpp
+++ include/Core.hpp
@@ -3,10 +3,30 @@
 #include <bit>
 #include <cstdint>
 
+#ifdef __cpp_lib_debugging
+#include <debugging>
+#endif
+
 namespace gb {
 
 using u8 = std::uint8_t;
 using u16 = std::uint16_t;
+
 using byte = u8;
+using u64 = std::uint64_t;
 
 }
+
+#ifdef DEBUG
+
+#ifdef __cpp_lib_debugging // c++26
+#define BREAKPOINT std::breakpoint_if_debugging()
+#elif defined(_MSC_VER)
+#define BREAKPOINT __debugbreak()
+#elif defined(__clang__)
+#define BREAKPOINT __builtin_debugtrap()
+#elif defined(__GNUC__) || defined(__GNUG__)
+#define BREAKPOINT __builtin_trap()
+#endif // breakpoint definitions
+
+#endif // DEBUG

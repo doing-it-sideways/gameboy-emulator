6892f7f33869557c858a5a9c5808fac58281dcab
doing-it-sideways <nohitruns73@gmail.com>
2025-04-10 03:41:39 -0400

    u32 type added

--- include/Core.hpp
+++ include/Core.hpp
@@ -12,9 +12,10 @@ namespace gb {
 
 using u8 = std::uint8_t;
 using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
 
 using byte = u8;
-using u64 = std::uint64_t;
 
 }
 
@@ -28,6 +29,6 @@ using u64 = std::uint64_t;
 #define BREAKPOINT __builtin_debugtrap()
 #elif defined(__GNUC__) || defined(__GNUG__)
 #define BREAKPOINT __builtin_trap()
-#endif // breakpoint definitions
+#endif // __cpp_lib_debugging -- breakpoint definitions
 
 #endif // DEBUG

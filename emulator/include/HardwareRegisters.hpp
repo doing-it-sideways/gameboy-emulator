468259aa2cb7e7ed8ac904cd497195c492c531fb
doing-it-sideways <nohitruns73@gmail.com>
2025-06-12 03:24:10 -0400

    added most of the graphics related hardware registers and added support for reading them

--- emulator/include/HardwareRegisters.hpp
+++ emulator/include/HardwareRegisters.hpp
@@ -6,46 +6,68 @@
 
 namespace gb {
 
+BITFIELD_UNION_BYTE(SerialControl, flags,
+	byte ClockSelect : 1;
+	byte ClockSpeed : 1;
+	byte _ : 5; // unused
+	byte TransferEnable : 1;
+);
+
+BITFIELD_UNION_BYTE(InterruptFlags, flags,
+	byte VBlankInt : 1;
+	byte LCDInt : 1;
+	byte TimerInt : 1;
+	byte SerialInt : 1;
+	byte JoypadInt : 1;
+	byte _ : 3; // unused
+);
+
+BITFIELD_UNION_BYTE(LCDControl, flags,
+	byte BGWindowEnable : 1;
+	byte OBJEnable : 1;
+	byte OBJSize : 1;
+	byte BGTileMapArea : 1;
+	byte BGWindowTilemapArea : 1;
+	byte WindowEnable : 1;
+	byte WindowTilemapArea : 1;
+	byte LCDEnable : 1;
+);
+
+BITFIELD_UNION_BYTE(LCDStatus, flags,
+	byte PPUMode : 2; // read only
+	byte LycEqLy : 1; // read only
+	byte M0Select : 1;
+	byte M1Select : 1;
+	byte M2Select : 1;
+	byte LycIntSelect : 1;
+	byte _ : 1; // unused
+);
+
 struct HWRegs {
-// ----- Structs and Typedefs -----
-	BITFIELD_UNION_BYTE(SerialControl, flags,
-		byte ClockSelect : 1;
-		byte ClockSpeed : 1;
-		byte _ : 5; // unused
-		byte TransferEnable : 1;
-	);
-
-	BITFIELD_UNION_BYTE(InterruptFlags, flags,
-		byte VBlankInt : 1;
-		byte LCDInt : 1;
-		byte TimerInt : 1;
-		byte SerialInt : 1;
-		byte JoypadInt : 1;
-		byte _ : 3; // unused
-	);
-
-// ----- Vars -----
 	// In order of their memory locations:
-
 	byte sb;			// $FF01 -- Serial Transfer Data
-
 	SerialControl sc;	// $FF02
 
 	Timer& timer;		// [$FF04, $FF07]
 
 	InterruptFlags iF;	// $FF0F
 
+	LCDControl lcdc;	// $FF40
+	LCDStatus stat;		// $FF41
+	byte scy;			// $FF42 -- Background viewport y pos
+	byte scx;			// $FF43 -- Background viewport x pos
+	byte ly;			// $FF44 -- LCD Y coordinate (read-only)
+	byte lyc;			// $FF45 -- LY compare
+	byte wy;			// $FF4A -- Window y pos
+	byte wx;			// $FF4B -- Window x pos + 7
+
 	InterruptFlags ie;	// $FFFF
 
 // ----- Funcs -----
 	static HWRegs InitRegs(Timer& emuTimer, bool isCGB = false);
-
-	void Reset(byte oldOAMReg, bool isCGB = false);
 
 	byte& Read(u16 addr);
 	void Write(u16 addr, byte val);
 };
 
 } // namespace gb
-
-#undef BITFIELD_UNION_BYTE
--- emulator/src/HardwareRegisters.cpp
+++ emulator/src/HardwareRegisters.cpp
@@ -12,18 +12,22 @@ HWRegs HWRegs::InitRegs(Timer& emuTimer, [[maybe_unused]] bool isCGB) {
 		.sc = 0x7E,
 		.timer = emuTimer,
 		.iF = 0xE1,
+		.lcdc = 0x91,
+		.stat = 0x85,
+		.scy = 0x00,
+		.scx = 0x00,
+		.ly = 0x00,
+		.lyc = 0x00,
+		.wy = 0x00,
+		.wx = 0x00,
 		.ie = 0x00
 	};
-}
-
-void HWRegs::Reset([[maybe_unused]] byte oldOAMReg, [[maybe_unused]] bool isCGB) {
-	// TODO
-	debug::cexpr::println(stderr, "hardware registers not reset");
 }
 
 byte& HWRegs::Read(u16 addr) {
 	static byte unimplByte = 0;
 	static byte gameboyDoctorLCDTemp = 0x90;
+
 	if (addr >= 0xFF04 && addr <= 0xFF07)
 		return timer.Read(addr);
 
@@ -31,7 +35,14 @@ byte& HWRegs::Read(u16 addr) {
 	case 0xFF01: return sb;
 	case 0xFF02: return sc.asByte;
 	case 0xFF0F: return iF.asByte;
+	case 0xFF40: return lcdc.asByte;
+	case 0xFF41: return stat.asByte;
+	case 0xFF42: return scy;
+	case 0xFF43: return scx;
 	case 0xFF44: return gameboyDoctorLCDTemp; // gameboy doctor tests TODO: remove
+	case 0xFF45: return lyc;
+	case 0xFF4A: return wy;
+	case 0xFF4B: return wx;
 	case 0xFFFF: return ie.asByte;
 	default:
 		debug::cexpr::println(stderr, "Unimplemented or invalid io read at {:#06x}", addr);

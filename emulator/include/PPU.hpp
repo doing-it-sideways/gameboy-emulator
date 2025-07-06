9232b3db021440a833e1a733e29fbfb32c82e79b
doing-it-sideways <nohitruns73@gmail.com>
2025-07-06 04:40:50 -0400

    initial work on ppu. Mode enum added, Palette enum added, constant expressions for the 4 types of gray colors added. Update function added. many constants added for convenient reference when drawing to the screen. PPU mode updating implemented. LY register updating behavior done.

--- emulator/include/PPU.hpp
+++ emulator/include/PPU.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <array>
+#include <glm/vec3.hpp>
 
 #include "Core.hpp"
+#include "HardwareRegisters.hpp"
 
 namespace gb {
 
@@ -10,22 +12,55 @@ class Memory;
 
 namespace ppu {
 
+enum class Mode : byte {
+	HBLANK = 0,
+	VBLANK = 1,
+	OAM_SCAN = 2,
+	PIXEL_DRAW = 3
+};
+
+enum class Palette : byte {
+	BGP = 0,
+	OBP0 = 1,
+	OBP1 = 2
+};
+
 class GContext {
-// --- Structs and Typedefs ---
 public:
-	struct ControlReg {
-
-	};
-
-	struct StatusReg {
-
+	static constexpr std::array<glm::vec3, 4> dmgColors = {
+		glm::vec3{ 1 },
+		glm::vec3{ .6666f },
+		glm::vec3{ .3333f },
+		glm::vec3{ 0 }
 	};
 
 public:
 	explicit GContext(Memory& memory);
 
+	bool Update();
+
+	Mode GetMode() const;
+	void SetMode(Mode newMode);
+
+	PaletteData GetPaletteData(Palette palette) const;
+	void SetPaletteData(Palette palette, PaletteData newIndices);
+
 private:
+	void UpdateLine(byte& ly);
+
+private:
+	static constexpr u16 lineMax = 153;
+	static constexpr u16 vBlankStartNext = 143;
+	static constexpr u16 dotsPerLine = 456;
+
+	static constexpr u16 hBlankMaxDots = 376 - 172;
+	static constexpr u16 oamScanDots = 80;
+	static constexpr u16 pixelDrawMaxDots = 289;
+
 	Memory& _memory;
+
+	//u16 _curLine = 0; use ly register
+	u16 _curDot = 0;
 };
 
 } // namespace ppu
--- emulator/src/PPU.cpp
+++ emulator/src/PPU.cpp
@@ -5,6 +5,79 @@ namespace gb::ppu {
 
 GContext::GContext(Memory& memory)
 	: _memory(memory)
-{}
+{
+	SetMode(Mode::OAM_SCAN);
+}
+
+bool GContext::Update() {
+	// ly should only ever be updated inside the ppu
+	byte& ly = _memory.Read(0xFF44);
+
+	switch (GetMode()) {
+	case Mode::HBLANK:
+		if (_curDot == dotsPerLine) {
+			_curDot = 0;
+
+			if (ly == vBlankStartNext)
+				SetMode(Mode::VBLANK);
+
+			UpdateLine(ly);
+			break;
+		}
+
+		break;
+	case Mode::VBLANK:
+		break;
+	case Mode::OAM_SCAN:
+		if (_curDot == oamScanDots)
+			SetMode(Mode::PIXEL_DRAW);
+		
+		++_curDot;
+		break;
+	case Mode::PIXEL_DRAW:
+		break;
+	}
+
+	return true;
+}
+
+void GContext::UpdateLine(byte& ly) {
+	++ly;
+
+	byte lycEqLy = static_cast<byte>(ly == _memory.Read(0xFF45));
+	byte& stat = _memory.Read(0xFF41);
+
+	// LCDStatus::LycEqLy should only be set here
+	if (lycEqLy != 0) {
+		LCDStatus statFlags = static_cast<LCDStatus>(stat);
+		statFlags.flags.LycEqLy = lycEqLy;
+
+		if (statFlags.flags.LycIntSelect == 1)
+			_memory.GetInterruptFlag().flags.LCDInt = 1;
+
+		stat = statFlags;
+	}
+	else
+		stat &= ~(1 << 2); // LycEqLy = 0
+}
+
+Mode GContext::GetMode() const {
+	return static_cast<Mode>(static_cast<LCDStatus>(_memory[0xFF41]).flags.PPUMode);
+}
+
+void GContext::SetMode(Mode newMode) {
+	byte& stat = _memory.Read(0xFF41);
+	
+	stat &= ~3; // clear bits first
+	stat |= static_cast<byte>(newMode); // then set to new value
+}
+
+PaletteData GContext::GetPaletteData(Palette palette) const {
+	return static_cast<PaletteData>(_memory[0xFF47 + static_cast<byte>(palette)]);
+}
+
+void GContext::SetPaletteData(Palette palette, PaletteData newIndices) {
+	_memory[0xFF47 + static_cast<byte>(palette)] = newIndices;
+}
 
 } // namespace gb::ppu
